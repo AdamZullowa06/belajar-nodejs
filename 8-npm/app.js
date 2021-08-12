ea1005c905ed1c24690a0a0393dd2e04d063dd9f
AdamZullowa06 <adamzullowa06@gmail.com>
2021-08-13 00:35:40 +0700

    update app.js

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('adamzullowa@gmail.com'));
+// console.log(validator.isMobilePhone('08111139312', 'id-ID'));
+// console.log(validator.isNumeric('123456789'));
+
+// console.log(chalk.italic.bgBlue.black('Hello World!'));
+const nama = 'John Doe';
+const pesan = chalk`Lorem ipsum dolor {bgBlue sit amet} consectetur {bgGreen.italic.black adipisicing} elit. Suscipit, accusamus?, Nama saya : ${nama}`;
+console.log(pesan);
